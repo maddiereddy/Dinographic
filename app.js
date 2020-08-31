9077b24f405bc356ce0d83fbbdd93cf6f8d374c6
Maddie Rajavasireddy <maddeireddy@gmail.com>
2020-08-30 17:16:59 -0700

    feat: added the objects to be used in the app

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 
     // Create Dino Constructor
+    function Dino(species, weight, height, diet, where, when, fact) {
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.diet = diet;
+        this.where = where;
+        this.when = when;
+        this.fact = fact;
+    }
 
 
     // Create Dino Objects
 
 
     // Create Human Object
+    function Human (name, weight, height, diet) {
+        return {
+            name: name,
+            weight: weight,
+            height: height,
+            diet: diet
+        };
+    }
 
     // Use IIFE to get human data from form
+    const human = (function () {
+
+        let name = document.getElementById('name').value;
+        let feet = document.getElementById("feet").value;
+        let inches = document.getElementById("inches").value;
+        let weight = document.getElementById('weight').value;
+        let diet = document.getElementById('diet').value;
+
+        function getName() {
+            return name;
+        }
+        function getHeight() {
+            feet = Number(feet);
+            inches = Number(inches);
+            finalHeight = ( feet * 12 ) + inches;
+            return finalHeight;
+        }
+        function getWeight() {
+            return weight; 
+        }
+        function getDiet() {
+            return diet;
+        }
+        return {
+            name: getName(),
+            height: getHeight(),
+            weight: getWeight(),
+            diet: getDiet()
+        };
+    })();
 
 
     // Create Dino Compare Method 1
@@ -30,3 +77,7 @@
 
 
 // On button click, prepare and display infographic
+document.getElementById('btn').addEventListener('click', (event) => {
+    let user = Human(human.name, human.height, human.weight, human.diet);
+    console.log(user.name);
+});
